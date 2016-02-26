d5d430dfc78cb1d87de9fce780deae4b7e27e81c
Adewale Oluyinka <davifantasia@hotmail.com>
2016-02-26 19:31:12 +0100

    App name changed, ads banner shows at buttom of screen

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,8 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter-controllers']).run(function ($ionicPlatform) {
+angular.module('starter', ['ionic', 'starter-controllers'])
+.run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
@@ -12,6 +13,8 @@ angular.module('starter', ['ionic', 'starter-controllers']).run(function ($ionic
         if (window.StatusBar) {
             StatusBar.styleDefault();
         }
+            
+        initializeAds();
     });
 }).config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider.state('tabs', {
@@ -53,3 +56,76 @@ angular.module('starter', ['ionic', 'starter-controllers']).run(function ($ionic
 
     $urlRouterProvider.otherwise('/tab/home');
 });
+
+function initializeAds() {
+    if (isMobileDevice()) continueWithAdsInitialization();
+}
+
+function isMobileDevice() {
+    return ionic.Platform.isIPad()
+    || ionic.Platform.isIOS()
+    || ionic.Platform.isAndroid()
+    || ionic.Platform.isWindowsPhone();
+}
+
+function continueWithAdsInitialization() {
+    // Device is mobile
+    console.log("It is Mobile");
+    
+    var admobid = getAdmobId();
+    
+    // Add admob banner ads
+    createBannerAd(admobid);
+    
+    // Add admob interstitial ads
+    // prepare and load ad resource in background
+    // if(AdMob) AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
+
+    // show the interstitial later, e.g. at end of game level
+    // if(AdMob) AdMob.showInterstitial();
+    // END: Add admob interstitial ads
+}
+
+function getAdmobId() {
+    // Add admob banner ads
+    var admobid = {};
+    
+    if (/(android)/i.test(navigator.userAgent)) { // for android & amazon-fireos
+        admobid = getAndroidAdmobId();
+    } else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) { // for ios
+        admobid = getIOSAdmobId();
+    } else { // for windows phone
+        admobid = getWindowsAdmobId();
+    }
+    
+    return admobid;
+}
+
+function getAndroidAdmobId(){
+    return {
+            banner: 'ca-app-pub-3940256099942544/6300978111', // or DFP format "/6253334/dfp_example_ad"
+            interstitial: 'ca-app-pub-3940256099942544/1033173712'
+        };
+}
+
+function getIOSAdmobId() {
+    return {
+            banner: 'ca-app-pub-3940256099942544/6300978111', // or DFP format "/6253334/dfp_example_ad"
+            interstitial: 'ca-app-pub-3940256099942544/1033173712'
+        };
+}
+
+function getWindowsAdmobId() {
+    return {
+            banner: 'ca-app-pub-3940256099942544/6300978111', // or DFP format "/6253334/dfp_example_ad"
+            interstitial: 'ca-app-pub-3940256099942544/1033173712'
+        };
+}
+
+function createBannerAd(admobid) {
+    // it will display smart banner at bottom center, using the default options
+    if(AdMob) AdMob.createBanner( {
+        adId: admobid.banner, 
+        position: AdMob.AD_POSITION.BOTTOM_CENTER, 
+        autoShow: true } );
+}
